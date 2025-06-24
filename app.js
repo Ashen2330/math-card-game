90640300718590e1244b0c5b3d60df6157ab6a0f
Ashen2330 <mactooatt@gmail.com>
2025-06-24 23:56:14 +0800

    Done with window location
    next is socketio

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-const express = require('express');
+import express from 'express'
 const app = express();
-const http = require('http');
+import http from 'http';
 const server = http.createServer(app);
-const path = require("path")
+import path from 'path';
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dbFile = path.join(__dirname, 'db.json');
+const adapter = new JSONFile(dbFile);
+
+const defaultData = {
+  User: 'default',
+  MasterVolume: 100,
+  BackgroundMusic: 100,
+  GameVolume: 100,
+  AnimationSpeed: 1
+};
+
+const db = new Low(adapter, defaultData);
 
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname , "template", "index.html"));
-  });
+});
+
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname , "template", "index.html"));
+});
+
+
+app.get('/room.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'template', 'room.html'));
+});
+
+app.get('/setting.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'template', 'setting.html'));
+});
   
 server.listen(3000, () => {
   console.log('listening on http://localhost:3000 ');
